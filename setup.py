6c48fdc0a51a45aed044ee5771294ffa8bb76fd8
mallikarjuna pokuri <mallikarjunapokuri595@gmail.com>
2023-11-08 08:23:33 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+
+
+setup(name = "Student Score Predictor",
+      version = "0.0.1",
+      description="Using Scikit-Learn Machine Learning algorithms to predict student scores",
+      author="Mallikarjuna Pokuri",
+      author_email="mallikarjunapokuri595@gmail.com")
+
